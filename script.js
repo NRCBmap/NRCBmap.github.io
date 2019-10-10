63c1521a6addead670ddc79b72d903ce64f4e6c7
CodeMaker_4 <thijs.rutledge@coderclass.nl>
2019-10-10 13:36:34 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var xScreenSize = innerWidth - 5; // canvas size
+var yScreenSize = innerHeight - 5;
+
+function setup() { // p5 setup
+  createCanvas(xScreenSize, yScreenSize);
+  background(255);
+}
+
+function draw() {;
+
+}
