8082a73719f7837c47e9469ee598a07ccdd09dd6
CodeMaker_4 <thijs.rutledge@coderclass.nl>
2020-03-11 11:09:25 +0100

    now using python

---
+++ discordBot/bot.py
@@ -0,0 +1,20 @@
+import discord
+import json
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+client = MyClient()
+
+f = open("auth.json", "r")
+client.run(json.loads(f.read())["token"])
+f.close()
